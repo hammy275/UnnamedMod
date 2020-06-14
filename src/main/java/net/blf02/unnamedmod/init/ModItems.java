a99ef04c5e25d4f0b4e300f49679b3165cfaafbf
hammy3502 <hammy275@gmail.com>
2020-06-14 00:09:33 -0400

    Unused imports

--- src/main/java/net/blf02/unnamedmod/init/ModItems.java
+++ src/main/java/net/blf02/unnamedmod/init/ModItems.java
@@ -3,25 +3,8 @@ package net.blf02.unnamedmod.init;
 import java.util.ArrayList;
 import java.util.List;
 
-import net.blf02.unnamedmod.UnnamedMod;
 import net.blf02.unnamedmod.items.ItemBase;
-import net.blf02.unnamedmod.items.armor.ArmorBase;
-import net.blf02.unnamedmod.items.tools.ToolAxe;
-import net.blf02.unnamedmod.items.tools.ToolHoe;
-import net.blf02.unnamedmod.items.tools.ToolPickaxe;
-import net.blf02.unnamedmod.items.tools.ToolSpade;
-import net.blf02.unnamedmod.items.tools.ToolSword;
-import net.minecraft.init.SoundEvents;
-import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.Item;
-import net.minecraft.item.Item.ToolMaterial;
-import net.minecraft.item.ItemArmor.ArmorMaterial;
-import net.minecraft.item.ItemAxe;
-import net.minecraft.item.ItemHoe;
-import net.minecraft.item.ItemPickaxe;
-import net.minecraft.item.ItemSpade;
-import net.minecraft.item.ItemSword;
-import net.minecraftforge.common.util.EnumHelper;
 
 public class ModItems {
 	
